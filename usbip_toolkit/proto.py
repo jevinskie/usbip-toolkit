adb243a37f354ae167bdb58237c03db0d3fba199
Jevin Sweval <jevinsweval@gmail.com>
2022-05-24 11:59:24 -0700

    wip

--- usbip_toolkit/proto.py
+++ usbip_toolkit/proto.py
@@ -78,7 +78,7 @@ USBInterface = Struct(
     "bInterfaceClass" / Int8ub,
     "bInterfaceSubclass" / Int8ub,
     "bInterfaceProtocol" / Int8ub,
-    Padding(8)
+    Padding(1)
 )
 
 OpCommon = Struct(
@@ -151,43 +151,55 @@ OpRequest = Struct(
     })
 )
 
+CMD_SUBMIT = 1
+CMD_UNLINK = 2
+RET_SUBMIT = 3
+RET_UNLINK = 4
 
 
-CommonHdr = (
-    "command" / Int32ub,
-    "seqnum" / Int32ub,
-    "devid" / Int32ub,
-    "direction" / Int32ub,
-    "ep" / Int32ub
-)
+def CommonHdr(cmd):
+    return (
+        "command" / Const(cmd, Int32ub),
+        "seqnum" / Int32ub,
+        "devid" / Int32ub,
+        "direction" / Int32ub,
+        "ep" / Int32ub
+    )
 
 CmdSubmitHdr = Struct(
-    *CommonHdr,
+    *CommonHdr(CMD_SUBMIT),
     "transfer_flags" / Int32ub,
     "transfer_buffer_lenth" / Int32sb,
-    "start_frame" / Int32sb,
-    "number_of_packets" / Int32sb,
+    "start_frame" / Const(0, Int32sb), # ISO not supported
+    "number_of_packets" / Const(0, Int32sb), # ISO not supported
     "interval" / Int32sb,
-    "setup" / Bytes(8)
+    "setup" / Bytes(8),
+    "transfer_buffer" / Bytes(this.transfer_buffer_length),
+    # iso_packet_descriptor not used/supported
 )
 
 RetSubmitHdr = Struct(
-    *CommonHdr,
+    *CommonHdr(RET_SUBMIT),
     "status" / Int32sb,
     "actual_length" / Int32sb,
-    "start_frame" / Int32sb,
-    "number_of_packets" / Int32sb,
-    "error_count" / Int32sb
+    "start_frame" / Const(0, Int32sb), # ISO not supported
+    "number_of_packets" / Const(0, Int32sb), # ISO not supported
+    "error_count" / Int32sb,
+    Padding(8),
+    "transfer_buffer" / Bytes(this.actual_length * this.direction),
+    # iso_packet_descriptor not used/supported
 )
 
 CmdUnlinkHdr = Struct(
-    *CommonHdr,
-    "seqnum" / Int32ub
+    *CommonHdr(CMD_UNLINK),
+    "seqnum" / Int32ub,
+    Padding(24)
 )
 
 RetUnlinkHdr = Struct(
-    *CommonHdr,
-    "status" / Int32sb
+    *CommonHdr(CMD_SUBMIT),
+    "status" / Int32sb,
+    Padding(24)
 )
 
 # fmt: on
