8770584285ed83a425e66974919506f09c08e844
Jevin Sweval <jevinsweval@gmail.com>
2022-05-31 15:25:25 -0700

    wip

--- usbip_toolkit/sim_bridge_rx.py
+++ usbip_toolkit/sim_bridge_rx.py
@@ -1,6 +1,7 @@
 import socket
 from itertools import count
 
+import reactivex
 import reactivex as rx
 from reactivex import operators as ops
 
@@ -12,6 +13,14 @@ class USBIPSimBridgeServer_rx:
         self.usbip_port = usbip_port
         self.sim_port = sim_port
         self.counter = count()
+        self.serv_sock = serv_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        serv_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+        serv_sock.bind(("localhost", sim_port))
+        serv_sock.listen(1)
+        print("waiting for incomming connection")
+        self.s, _ = serv_sock.accept()
+        print("got connection")
 
     def serve(self):
         print("server running")
@@ -20,3 +29,21 @@ class USBIPSimBridgeServer_rx:
 
     def tcp_echo_server(self):
         print("starting server")
+
+        def rx_loop(observer, scheduler):
+            while True:
+                buf = self.s.recv(1024)
+                if buf:
+                    observer.on_next(buf)
+                else:
+                    print("got empty buf completing")
+                    observer.on_completed()
+                    break
+
+        source = rx.create(rx_loop)
+
+        source.subscribe(
+            on_next=lambda i: print("Received {0}".format(i)),
+            on_error=lambda e: print("Error Occurred: {0}".format(e)),
+            on_completed=lambda: print("Done!"),
+        )
