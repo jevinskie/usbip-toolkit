964e936d0207be56571dab1022753287f13188dc
Jevin Sweval <jevinsweval@gmail.com>
2022-05-30 17:27:02 -0700

    LengthPrefixedReceiver

--- usbip_toolkit/demo.py
+++ usbip_toolkit/demo.py
@@ -3,6 +3,7 @@ from itertools import count
 import trio
 
 from usbip_toolkit.proto import *
+from usbip_toolkit.util import LengthPrefixedReceiver
 
 
 class DemoServer:
@@ -10,19 +11,44 @@ class DemoServer:
         self.port = port
         self.counter = count()
 
+    async def serve_usbip_async(self):
+        await trio.serve_tcp(self.usbip_server, self.port)
+
+    async def serve_utmi_async(self):
+        await trio.serve_tcp(self.utmi_server, 2443)
+
     async def serve_async(self):
-        await trio.serve_tcp(self.demo_server, self.port)
+        # await trio.serve_tcp(self.demo_server, self.port)
+        print("serve_async begin")
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(self.serve_usbip_async)
+            nursery.start_soon(self.serve_utmi_async)
+        print("serve_async end")
 
     def serve(self):
         trio.run(self.serve_async)
 
-    async def demo_server(self, server_stream):
+    async def utmi_server(self, server_stream):
+        ident = next(self.counter)
+        print(f"utmi_server {ident}: started")
+        framed_server_stream = LengthPrefixedReceiver(server_stream)
+        try:
+            async for data in framed_server_stream:
+                print(f"utmi_server {ident}: received data {data.hex()}")
+
+                smsg = b"\x00"
+                await server_stream.send_all(smsg)
+            print(f"utmi_server {ident}: connection closed")
+        except Exception as exc:
+            print(f"utmi_server {ident}: crashed: {exc!r}")
+
+    async def usbip_server(self, server_stream):
         ident = next(self.counter)
         print(f"demo_server {ident}: started")
         try:
             i = 0
             async for data in server_stream:
-                print(f"demo_server {ident}: received data {data.hex()}")
+                print(f"usbip_server {ident}: received data {data.hex()}")
                 if i == 0:
                     cmsg = OpRequest.parse(data)
                     print(f"cmsg: {cmsg}")
@@ -56,6 +82,6 @@ class DemoServer:
                     print(f"smsg: {smsg.hex()}")
                 await server_stream.send_all(smsg)
                 i += 1
-            print(f"demo_server {ident}: connection closed")
+            print(f"usbip_server {ident}: connection closed")
         except Exception as exc:
-            print(f"demo_server {ident}: crashed: {exc!r}")
+            print(f"usbip_server {ident}: crashed: {exc!r}")
